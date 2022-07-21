4353555551b2e4e637cb92459fab0909881a68d3
Chad Stark <chad.d.stark@hotmail.com>
2022-07-20 22:25:34 -0700

    added basic login for discord bot

---
+++ discord/bot.tsx
@@ -0,0 +1,13 @@
+const fs = require("node:fs");
+const path = require("node:path");
+require("dotenv").config();
+
+const client = require("./client.tsx");
+
+
+client.login(process.env.DISCORD_LOGIN);
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
