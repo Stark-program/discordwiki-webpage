25e89bcabdfde6aa8e2059a7ebbc2abfaedf33bc
Chad Stark <chad.d.stark@hotmail.com>
2022-08-04 23:54:55 -0700

    changed types of the functional arguments

--- discord/commands/getmessages.tsx
+++ discord/commands/getmessages.tsx
@@ -14,13 +14,13 @@ module.exports = {
   async execute(interaction: interactionType) {
     const guildId = interaction.guildId
     const channelId = interaction.channelId
-    const messages: any = [];
+    const messages: Array<any> = [];
     const channel = client.channels.cache.get(channelId)
     
     //set initial message pointer to the last message sent in the channel
     let message = await channel.messages
       .fetch({ limit: 1 })
-      .then((messagePage: any) => {
+      .then((messagePage: interactionType) => {
         if (messagePage.size === 1) {
           return messagePage
         } else return null 
@@ -29,8 +29,8 @@ module.exports = {
     while (message) {
       await channel.messages
         .fetch({ limit: 100, before: message.id })
-        .then((messagePage: any) => { 
-          messagePage.forEach((msg: any) => messages.push(msg));
+        .then((messagePage: interactionType) => { 
+          messagePage.forEach((msg: interactionType) => messages.push(msg));
         
           //set new message pointer
           message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
