b3c1a3f78d8e4be2448a645354b7a949963cb924
Chad Stark <chad.d.stark@hotmail.com>
2022-08-25 11:43:57 -0700

    added comments explaining logic

--- pages/guilds/[slug].tsx
+++ pages/guilds/[slug].tsx
@@ -4,24 +4,25 @@ const axios = require("axios").default;
 
 export default function Guild() {
   const router = useRouter();
-
   const [guildChannels, setGuildChannels] = useState();
   const [haveData, setHaveData] = useState(false);
 
+  //check if the router query is ready to be used, then fetch the data
   useEffect(() => {
     if (router.isReady) {
       const { slug } = router.query;
-
       getGuildData(slug);
     }
   }, [router.isReady]);
 
+  //retrieves specific guild channel information
   async function getGuildData(slug: any) {
     const response = await axios.get(`http://localhost:3000/guilds/${slug}`);
     setGuildChannels(response.data.channels);
     setHaveData(true);
   }
 
+  //renders the channels for the specific guild page
   function renderChannels(channels: Array<object>) {
     console.log(channels);
     const render = channels.map((channel: any) => {
@@ -29,6 +30,7 @@ export default function Guild() {
     });
     return <ul>{render}</ul>;
   }
+
   return (
     <div>
       <h1>
