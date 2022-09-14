e5b29c4cbfdf0d5267206d294fd138844e2148bb
Chad Stark <chad.d.stark@hotmail.com>
2022-09-14 12:05:54 -0700

    added a particle background to landing page

---
+++ components/messages/particles.tsx
@@ -0,0 +1,58 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import { loadFull } from "tsparticles";
+import { useCallback, useMemo } from "react";
+
+export default function ParticlesComponent() {
+  const particlesOptions = useMemo(() => {
+    return {
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10,
+          },
+          repulse: {
+            distance: 100,
+            duration: 0.4,
+            radius: 100,
+          },
+        },
+      },
+
+      background: {
+        color: "#2C2F33",
+      },
+      particles: {
+        links: {
+          enable: true,
+        },
+        move: {
+          enable: true,
+        },
+        opacity: {
+          value: { min: 0.1, max: 1 },
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+        fullScreen: {
+          enable: true,
+
+          zIndex: 0,
+        },
+      },
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine: any) => {
+    return loadSlim(engine);
+  }, []);
+
+  return <Particles init={particlesInit} options={particlesOptions} />;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,94 +2,24 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { tsParticles } from "tsparticles-engine";
 import { SiDiscord } from "react-icons/si";
-
-// tsParticles.load("tsparticles", {
-//   background: {
-//     color: "#0d47a1",
-//   },
-//   interactivity: {
-//     events: {
-//       onClick: {
-//         enable: true,
-//         mode: "push",
-//       },
-//       onHover: {
-//         enable: true,
-//         mode: "repulse",
-//       },
-//       resize: true,
-//     },
-//     modes: {
-//       bubble: {
-//         distance: 400,
-//         duration: 2,
-//         opacity: 0.8,
-//         size: 40,
-//       },
-//       push: {
-//         quantity: 4,
-//       },
-//       repulse: {
-//         distance: 200,
-//         duration: 0.4,
-//       },
-//     },
-//   },
-//   particles: {
-//     color: {
-//       value: "#ffffff",
-//     },
-//     links: {
-//       color: "#ffffff",
-//       distance: 150,
-//       enable: true,
-//       opacity: 0.5,
-//       width: 1,
-//     },
-//     collisions: {
-//       enable: true,
-//     },
-//     move: {
-//       direction: "none",
-//       enable: true,
-//       outMode: "bounce",
-//       random: false,
-//       speed: 6,
-//       straight: false,
-//     },
-//     number: {
-//       density: {
-//         enable: true,
-//         value_area: 800,
-//       },
-//       value: 80,
-//     },
-//     opacity: {
-//       value: 0.5,
-//     },
-//     shape: {
-//       type: "circle",
-//     },
-//     size: {
-//       random: true,
-//       value: 5,
-//     },
-//   },
-// });
+import ParticlesComponent from "../components/messages/particles";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen w-screen justify-center items-center flex-col">
-      <div className="h-2/3 lg:h-1/2">
+    <div className="flex h-screen w-screen justify-center items-center flex-col ">
+      <div className="h-2/3 lg:h-1/2 z-40">
         <div className="flex flex-row">
           <span className="flex justify-center items-center">
-            <SiDiscord className="h-10 w-10 lg:h-16 lg:w-16" />
+            <SiDiscord
+              className="h-10 w-10 mr-4 lg:h-16 lg:w-16"
+              color="white"
+            />
           </span>
-          <h1 className="text-[60px] font-['PT_Sans'] lg:text-[100px]">
+          <h1 className="text-[60px] font-['PT_Sans'] lg:text-[100px] text-[#EAECEE] ">
             Discord Wiki
           </h1>
         </div>
-        <p className="text-center font-['PT_Sans'] lg:text-[20px]">
+        <p className="text-center font-['PT_Sans'] lg:text-[20px] text-[#EAECEE] ">
           Find your community.
         </p>
         <div className="flex justify-center mt-12 lg:mt-24">
@@ -100,6 +30,7 @@ const Home: NextPage = () => {
           </Link>
         </div>
       </div>
+      <ParticlesComponent />
     </div>
   );
 };
