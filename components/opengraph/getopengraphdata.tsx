75f539fb3a2b8bb38ae04c79120380f072db2ce7
Chad Stark <chad.d.stark@hotmail.com>
2022-09-26 15:25:05 -0700

    now links to respective URL when clicked

--- components/opengraph/getopengraphdata.tsx
+++ components/opengraph/getopengraphdata.tsx
@@ -32,28 +32,34 @@ export default function OpenGraph(props: { link: string }) {
   function checkToRenderOpenGraphCard() {
     if (openGraphData.title === undefined) {
       return (
-        <div className="flex flex-col max-w-2/3 lg:max-w-full ml-9 lg:ml-14 sm:w-1/2 md:w-1/3 ">
-          <p className="text-sky-500 truncate">{openGraphData.url}</p>
+        <div className="flex flex-col max-w-2/3 cursor-pointer lg:max-w-full ml-9 lg:ml-14 sm:w-1/2 md:w-1/3 ">
+          <p className="text-sky-500 truncate ">
+            <a href={openGraphData.url} target="_blank">
+              {openGraphData.url}
+            </a>
+          </p>
         </div>
       );
     } else {
       return (
-        <div className="flex flex-col max-w-2/3 lg:max-w-full ml-9 lg:ml-14 sm:w-1/2 md:w-1/3 ">
-          <p className="text-sky-500 truncate">{openGraphData.url}</p>
-          <div className=" bg-DW-gray max-h-[600px] rounded">
-            <div className="flex flex-col mx-4 h-full">
-              <h1 className="text-sky-500 mt-2 font-Baskerville">
-                {openGraphData.title}
-              </h1>
-              <p className="h-1/3 break-words text-DW-white mr-4 mt-2">
-                {openGraphData.description}
-              </p>
-              <img
-                src={openGraphData.image}
-                className="max-h-[400px] w-full mt-2 mb-4 rounded"
-              />
+        <div className="flex flex-col max-w-2/3  ml-9 cursor-pointer lg:max-w-full lg:ml-14 sm:w-1/2 md:w-1/3 ">
+          <a href={openGraphData.url} target="_blank">
+            <p className="text-sky-500 truncate">{openGraphData.url}</p>
+            <div className=" bg-DW-gray max-h-[600px] rounded">
+              <div className="flex flex-col mx-4 h-full">
+                <h1 className="text-sky-500 mt-2 font-Baskerville">
+                  {openGraphData.title}
+                </h1>
+                <p className="h-1/3 break-words text-DW-white mr-4 mt-2">
+                  {openGraphData.description}
+                </p>
+                <img
+                  src={openGraphData.image}
+                  className="max-h-[400px] w-full mt-2 mb-4 rounded"
+                />
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       );
     }
