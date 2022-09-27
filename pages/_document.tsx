0fd330d144b3ef063ea2acab68527207b5048019
Chad Stark <chad.d.stark@hotmail.com>
2022-09-26 20:47:31 -0700

    removed api, added meta og tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
 
-export default function Document() {
+export default function Application() {
   return (
     <Html>
       <Head>
@@ -8,6 +9,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Montserrat:wght@400;700&family=Work+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content="Discord Wiki" />
+        <meta property="og:image" content="https://imgur.com/a/W3XYCAk" />
+        <meta
+          property="og:description"
+          content="Discord Wiki is a community driven server browser aimed to give users an ability to see what communities are like, seamlessly and easily. You can checkout text channels and browse the content posted by that server!"
+        />
+        <meta property="og:url" content="https://discordwiki.us" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Discord Wiki" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@discordwiki" />
+        <meta name="twitter:creator" content="@discordwiki" />
+        <meta name="twitter:title" content="Discord Wiki" />
       </Head>
       <body>
         <Main />
