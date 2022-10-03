9a745345e6cb9f978b323038328d3183a9851d4c
Casey Cochran <Casey3362@yahoo.com>
2022-10-02 18:40:48 -0700

    same changes as before without yarn.lock modify

--- components/navbar/Nav.tsx
+++ components/navbar/Nav.tsx
@@ -8,17 +8,20 @@ export default function Nav(props: any) {
     <div className="flex justify-between py-4 text-xl lg:px-4 lg:py-6 md:text-2xl lg:text-4xl font-Baskerville text-DW-white bg-DW-darkGray">
       <div className="flex flex-row pl-1">
         <MobileMenuButton toggleMobileMenu={toggleMobileMenu} />
-
-        <p className="md:ml-4">Discord Wiki</p>
+        <Link href="/">
+          <p className="md:ml-4 hover:text-slate-300 transition ease-in-out delay-100 hover:cursor-pointer">
+            Discord Wiki
+          </p>
+        </Link>
       </div>
       <div className="flex justify-end">
         <Link href="/guilds">
-          <button className="px-4 text-sm md:text-lg lg:text-2xl font-WorkSans hover:cursor-pointer">
+          <button className="px-4 text-sm md:text-lg lg:text-2xl font-WorkSans hover:cursor-pointer hover:text-slate-300 transition ease-in-out delay-100">
             Guilds
           </button>
         </Link>
         <Link href="/download">
-          <button className="px-4 text-sm md:text-lg lg:text-2xl font-WorkSans hover:cursor-pointer">
+          <button className="px-4 text-sm md:text-lg lg:text-2xl font-WorkSans hover:cursor-pointer hover:text-slate-300 transition ease-in-out delay-100">
             Download
           </button>
         </Link>
